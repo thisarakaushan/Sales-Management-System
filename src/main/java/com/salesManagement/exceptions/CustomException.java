ee657ed5a2e39d5725334a602b33531a7e0d6d57
Thisara Kaushan <125348115+thisarakaushan@users.noreply.github.com>
2023-08-20 00:36:49 +0530

    Add files via upload

--- src/main/java/com/salesManagement/exceptions/CustomException.java
+++ src/main/java/com/salesManagement/exceptions/CustomException.java
@@ -1,1 +1,17 @@
+package com.highradius.salesManagement.exceptions;
 
+/**
+ * This method for handle the every exception that can be happen...
+ * @author thisara.kaushan
+ *
+ */
+public class CustomException extends Exception {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CustomException(String message) {
+        super(message);
+    }
+}
